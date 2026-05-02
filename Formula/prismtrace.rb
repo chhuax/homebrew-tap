b08b30af07e1741f341d4c5808fa73a2491f39ab
huaxin <huaxin@yonyou.com>
2026-05-02 15:57:50 +0800

    Update PrismTrace formula for Intel macOS

--- Formula/prismtrace.rb
+++ Formula/prismtrace.rb
@@ -1,11 +1,15 @@
 class Prismtrace < Formula
   desc "Local AI observability tool for macOS"
   homepage "https://github.com/chhuax/PrismTrace"
-  url "https://github.com/chhuax/PrismTrace/releases/download/v0.1.0-alpha.1/prismtrace-0.1.0-alpha.1-aarch64-apple-darwin.tar.gz"
-  sha256 "a9baab677ce9320fd161e2fff28020fbc2c06677c0d556839277831315cb3351"
   license "MIT"
 
-  depends_on arch: :arm64
+  if Hardware::CPU.arm?
+    url "https://github.com/chhuax/PrismTrace/releases/download/v0.1.0-alpha.2/prismtrace-0.1.0-alpha.2-aarch64-apple-darwin.tar.gz"
+    sha256 "782b9543293462fac77b6b7833960dfe2d6b66da2f63da3427372acceedba118"
+  else
+    url "https://github.com/chhuax/PrismTrace/releases/download/v0.1.0-alpha.2/prismtrace-0.1.0-alpha.2-x86_64-apple-darwin.tar.gz"
+    sha256 "c6c137662d4d185cfa80cab312f62c4af9d05eb1cb1ed755f620c2bf189e8cc7"
+  end
 
   def install
     bin.install "bin/prismtrace"
