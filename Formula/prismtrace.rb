b847732deb3dfb12372a7d3e935cde4e97026736
huaxin <huaxin@yonyou.com>
2026-05-03 15:02:12 +0800

    Update PrismTrace to v0.1.0-alpha.4

--- Formula/prismtrace.rb
+++ Formula/prismtrace.rb
@@ -4,11 +4,11 @@ class Prismtrace < Formula
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/chhuax/PrismTrace/releases/download/v0.1.0-alpha.3/prismtrace-0.1.0-alpha.3-aarch64-apple-darwin.tar.gz"
-    sha256 "0967c33a7518facb7570d1758988f48e718e321b19415d838058b1e16e334249"
+    url "https://github.com/chhuax/PrismTrace/releases/download/v0.1.0-alpha.4/prismtrace-0.1.0-alpha.4-aarch64-apple-darwin.tar.gz"
+    sha256 "123b488d3c010d2bc45b13b479cca320d06459e6637fd4f2c8f7243beac7978e"
   else
-    url "https://github.com/chhuax/PrismTrace/releases/download/v0.1.0-alpha.3/prismtrace-0.1.0-alpha.3-x86_64-apple-darwin.tar.gz"
-    sha256 "b94876637fe422af16f6f1176c44e7096dea18685f6cc9c83398ae1681e48a39"
+    url "https://github.com/chhuax/PrismTrace/releases/download/v0.1.0-alpha.4/prismtrace-0.1.0-alpha.4-x86_64-apple-darwin.tar.gz"
+    sha256 "fd93c9a85f251e2983c0f0e9b3c90025401c28897ee99b4f1f4737f1c1f6c794"
   end
 
   def install
